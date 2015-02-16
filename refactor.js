f18308109c7157d3275a3d1ef63a6b4b150fd890
Brian Leonard <brian@theleoreport.com>
2015-02-16 14:21:15 -0500

    question 4 complete

--- refactor.js
+++ refactor.js
@@ -114,6 +114,9 @@ var reverseStr = function(str) {
 
 // Put your answer below -------------------------
 
+var reverseStr = function(str) {
+  return str.split("").reverse().join("");
+}
 
 // -----------------------------------------------
 
